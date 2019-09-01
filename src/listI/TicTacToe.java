57ceb92d276d5a3b373f1f2a979ab21bfb977cfe
osor1o <osor1o@hotmail.com>
2019-09-01 03:09:34 -0300

    Finished Tic Tac Toe

--- src/listI/TicTacToe.java
+++ src/listI/TicTacToe.java
@@ -1,13 +1,86 @@
 package listI;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class TicTacToe extends JFrame {
 	private ArrayList<JButton> buttons = new ArrayList<JButton>();
+	private Listener listener = new Listener();
+	private int turn;
+	private final int MAX_TURN = 9;
 	
 	public TicTacToe() {
+		GridLayout grid = new GridLayout(3, 3);
+		this.setLayout(grid);
+		
+		for(int i = 0; i < 9; i++) {
+			buttons.add(new JButton());
+			JButton button = buttons.get(i);
+			this.add(button);
+			button.addActionListener(listener);
+		}
+		
+		this.pack();
+		this.setVisible(true);
+		this.setSize(300, 300);
+		this.setResizable(false);
+		this.setTitle("Tic Tac Toe");
+		this.initialState();
+	}
+	
+	private void initialState() {
+		turn = 1;
+		for(JButton button : buttons) {
+			button.setText("");
+		}
+	}
+	
+	private String getTurnSymbol() {
+		if(turn % 2 == 0)
+			return "X";
+		return "O";
+	}
+	
+	private boolean isWin() {
+		return (
+			isEqualsButtons(1, 2, 3) || isEqualsButtons(4, 5, 6) || isEqualsButtons(7, 8, 9) ||
+			isEqualsButtons(1, 4, 7) || isEqualsButtons(2, 5, 8) || isEqualsButtons(3, 6, 9) ||
+			isEqualsButtons(1, 5, 9) || isEqualsButtons(3, 5, 7)
+		);
+	}
+	
+	private boolean isEqualsButtons(int i1, int i2, int i3) {
+		String b1 = buttons.get(i1 - 1).getText();
+		String b2 = buttons.get(i2 - 1).getText();
+		String b3 = buttons.get(i3 - 1).getText();
+		return b1.equals(b2) && b2.equals(b3) && !b1.isEmpty();
+	}
+	
+	private void finished() {
+		if(isWin()) {
+			JOptionPane.showMessageDialog(null, "FINISHED! \"" + getTurnSymbol() +"\" WIN");
+			initialState();
+		} else if(turn == MAX_TURN) {
+			JOptionPane.showMessageDialog(null, "FINISHED! Draw");
+			initialState();
+		} else {
+			turn++;
+		}
+	}
+	
+	private class Listener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			for(JButton button : buttons) {
+				if(e.getSource() == button) {
+					button.setText(getTurnSymbol());
+				}
+			}
+			finished();
+		}
 	}
 }
